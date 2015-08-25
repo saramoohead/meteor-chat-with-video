bde898771c2aacc501580765f515a25f22cb2178
Sara OC <saramoo@hotmail.com>
2015-08-25 21:23:39 +0100

    create comment template and input form

---
+++ oc_meteor.js
@@ -0,0 +1,32 @@
+Comments = new Mongo.Collection("comments");
+
+if (Meteor.isClient) {
+
+    Template.comments.helpers({
+        commentList: function () {
+            return Comments.find();
+        }
+    });
+
+    Template.body.events({
+        "submit .new-comment": function (event) {
+            event.preventDefault();
+
+            var commentText = event.target.commentText.value;
+
+            Comments.insert({
+                commentText: commentText,
+                createdAt: new Date()
+            });
+
+            event.target.commentText.value = "";
+        }
+    });
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
